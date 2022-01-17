4a6742af558978c87de65f4cec6178ed54ae158c
ninjaco1 <nguyean6@users.noreply.github.com>
2022-01-16 21:34:37 -0800

    update

--- arduino/keyboard-serial/keyboard-serial.ino
+++ arduino/keyboard-serial/keyboard-serial.ino
@@ -1,13 +1,19 @@
 #include "./keyboard_serial_header.hpp"
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Key k; // create an key object which has all the functions in it
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      
+    }
+  }
 }
