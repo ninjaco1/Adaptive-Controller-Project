4b785795039e0dc101826ed42207c2e13be2c567
ninjaco1 <nguyean6@users.noreply.github.com>
2022-01-27 19:21:45 -0800

    debounce

--- arduino/keyboard-serial/keyboard-serial.ino
+++ arduino/keyboard-serial/keyboard-serial.ino
@@ -10,6 +10,14 @@ Key k; // create an key object which has all the functions in it
 // variables will change:
 int buttonState1 = 0; // variable for reading the pushbutton status
 int buttonState2 = 0; // variable for reading the pushbutton status
+int lastButtonState1 = LOW;
+int lastButtonState2 = LOW;
+
+// for debouncing
+unsigned long lastDebounceTime1 = 0; // the last time the output pin was toggled
+unsigned long lastDebounceTime2 = 0; // the last time the output pin was toggled
+unsigned long debouceDelay = 50; // the debounce time; increase if the output flickers
+
 
 // joysticks count
 uint8_t joystick1 = 0;
@@ -94,28 +102,82 @@ void loop()
 
   #ifdef BUTTON_CONTROL
   //  for marcos to see if they work
-  buttonState1 = digitalRead(buttonPin1);
-  buttonState2 = digitalRead(buttonPin2);
+  // buttonState1 = digitalRead(buttonPin1);
+  // buttonState2 = digitalRead(buttonPin2);
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState1 == HIGH) {
-    // open new tab:
-    // ctrl + t
-    k.keyboard_serial(17,25); // ctrl
-    k.keyboard_serial(11,24); // t
-    k.toggle_ctrl();
-    delay(1000);
-  } 
+  // // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  // if (buttonState1 == HIGH) {
+  //   // open new tab:
+  //   // ctrl + t
+  //   k.keyboard_serial(17,25); // ctrl
+  //   k.keyboard_serial(11,24); // t
+  //   k.toggle_ctrl();
+  //   delay(1000);
+  // } 
 
-  if (buttonState2 == HIGH){
-    // close window
-    // ctrl + w
-    k.keyboard_serial(17,25); // ctrl
-    k.keyboard_serial(12,22); // w
-    k.toggle_ctrl();
-    delay(1000);
+  // if (buttonState2 == HIGH){
+  //   // close window
+  //   // ctrl + w
+  //   k.keyboard_serial(17,25); // ctrl
+  //   k.keyboard_serial(12,22); // w
+  //   k.toggle_ctrl();
+  //   delay(1000);
+  // }
+  //****************************************************
+  // deboucing
+  // https://docs.arduino.cc/built-in-examples/digital/Debounce
+  int reading1, reading2;
+  
+  // read the state of the switch into a local variable
+  reading1 = digitalRead(buttonPin1);
+  reading2 = digitalRead(buttonPin2);
+
+  // check to see if you pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // if the switch changed, due to noise or pressing:
+  if(reading1 != lastButtonState1){
+    // reset the debouncing timer
+    lastDebouncetime1 = millis();
   }
 
+  if(reading2 != lastButtonState2){
+    // rest the bouncing timer
+    lastDebounceTime2 = millis();
+  }
+
+  if((millis() - lastDebounceTime1) > debouceDelay){
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so that it as the actual current state:
+
+    //if the button state has changed:
+    if (reading1 != buttonState1){
+      buttonState1 = reading1;
+
+      // only toggle thet LEDif the new button sttae is HIGH
+      if (buttonState1 == HIGH){
+        // do something
+        
+      }
+    }
+  }
+  if((millis() - lastDebounceTime2) > debouceDelay){
+    //if the button state has changed:
+    if (reading2 != buttonState2){
+      buttonState2 = reading2;
+
+      // only toggle thet LEDif the new button sttae is HIGH
+      if (buttonState2 == HIGH){
+        // do something
+        
+      }
+    }
+  }
+
+
+  lastButtonState1 = reading1;
+  lastButtonState2 = reading2;
   #endif
 
 
