574a9ff1d93253ffd21f039beec3ece42348c1f3
estensoa <56376870+estensoa@users.noreply.github.com>
2022-03-04 16:41:24 -0800

    Add files via upload
    Updated code to include joystick button debounce

--- joystick_buttons_block.ino
+++ joystick_buttons_block.ino
@@ -33,164 +33,197 @@ uint8_t Ctrl = OFF;
 uint8_t Alt = OFF;
 
 //******************************************************************************
-//                            chk_buttons                                      
-//Checks the state of the button number passed to it. It shifts in ones till   
-//the button is pushed. Function returns a 1 only once per debounced button    
-//push so a debounce and toggle function can be implemented at the same time.  
-//Adapted to check all buttons from Ganssel's "Guide to Debouncing"            
-//Expects active low pushbuttons on PINA port.  Debounce time is determined by 
+//                            chk_buttons
+//Checks the state of the button number passed to it. It shifts in ones till
+//the button is pushed. Function returns a 1 only once per debounced button
+//push so a debounce and toggle function can be implemented at the same time.
+//Adapted to check all buttons from Ganssel's "Guide to Debouncing"
+//Expects active low pushbuttons on PINA port.  Debounce time is determined by
 //external loop delay times 12. Looks for rising edge on button so held actions
 //have no unexpected effect.
 //******************************************************************************
 uint8_t chk_buttons(uint8_t pin) {
-        static uint16_t state = 0;      //holds state of debounce reading
-        state = (state << 1) | (digitalRead(pin)) | 0xE000; //mask on PINA button
-        if (state == 0xEFFF)    //compares to rising edge value (so holding doesn't stack)
-                return 1;
+  static uint16_t state = 0;      //holds state of debounce reading
+  state = (state << 1) | (digitalRead(pin)) | 0xE000; //mask on PINA button
+  if (state == 0xEFFF)    //compares to rising edge value (so holding doesn't stack)
+    return 1;
 
-        //if not true for 12 times, return false        
-        return 0;
+  //if not true for 12 times, return false
+  return 0;
+}
+
+//******************************************************************************
+//                            chk_click
+//Checks the state of the click passed to it. It shifts in ones till
+//the button is pushed. Function returns a 1 only once per debounced button
+//push so a debounce and toggle function can be implemented at the same time.
+//Adapted to check all buttons from Ganssel's "Guide to Debouncing"
+//Expects active low pushbuttons on PINA port.  Debounce time is determined by
+//external loop delay times 12. Looks for rising edge on button so held actions
+//have no unexpected effect.
+//******************************************************************************
+uint8_t chk_click(uint8_t which_click) {
+  static uint8_t state1 = 0;      //holds state of right debounce reading
+  static uint8_t state2 = 0;      //holds state of left debounce
+  if (which_click == 1) {  //right click
+    state1 = (state1 << 1) | (!Rstick.getButton()) | 0xE0; //mask
+    if (state1 == 0xEF)    //compares to rising edge value (so holding doesn't stack)
+      return 1;
+
+    //if not true for 4 times, return false
+    return 0;
+  }
+
+  if (which_click == 2) {  //left click
+    state2 = (state2 << 1) | (!Lstick.getButton()) | 0xE0; //mask
+    if (state2 == 0xEF)    //compares to rising edge value (so holding doesn't stack)
+      return 1;
+
+    //if not true for 4 times, return false
+    return 0;
+  }
 }
 
 
 void setup() {
   Serial.begin(9600);   //start comms at 9600 baud
-/*
-    if(Lstick.begin(Wire, Laddress) == false){
-    Serial.println("Left Joystick does not appear to be connected. Please check wiring. Freezing...");
-    while(1);
-    }
-*/   
-    if(Rstick.begin(Wire, Raddress) == false){
+  /*
+      if(Lstick.begin(Wire, Laddress) == false){
+      Serial.println("Left Joystick does not appear to be connected. Please check wiring. Freezing...");
+      while(1);
+      }
+  */
+  if (Rstick.begin(Wire, Raddress) == false) {
     Serial.println("Right Joystick does not appear to be connected. Please check wiring. Freezing...");
-    while(1);
-    }
-   
+    while (1);
+  }
 
-   //set D3-6 as inputs
-   pinMode(3, INPUT);
-   pinMode(4, INPUT);
-   pinMode(5, INPUT);
-   pinMode(6, INPUT);
+
+  //set D3-6 as inputs
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
 }
 
 void loop() {
   //gather coord data from joystick (0-1023)
-  
-  LinX = Lstick.getHorizontal();
-  LinY = Lstick.getVertical();
-  Lclick = !(Lstick.getButton());
-  
+  /*
+    LinX = Lstick.getHorizontal();
+    LinY = Lstick.getVertical();
+    Lclick = chk_click(2);    //send 2 for left click, 1 for right
+  */
   RinX = Rstick.getHorizontal();
   RinY = Rstick.getVertical();
-  Rclick = !(Rstick.getButton());
-  
+  Rclick = chk_click(1);    //send 1 for right click, 2 for left click
+
 
   //Parse joystick data for keyboard
+  /*
+    //LEFT STICK: if X is to the left
+    if(LinX < 470){  //about 60 (~20%) away from center to constitute change
+      //three cases for Y
+      if(LinY < 470){
+        Lkey = 15;
+      }
+      else if(LinY > 575){
+        Lkey = 17;
+      }
+      else{
+        Lkey = 16;
+      }
+    }
 
-  //LEFT STICK: if X is to the left
-  if(LinX < 470){  //about 60 (~20%) away from center to constitute change
-    //three cases for Y
-    if(LinY < 470){
-      Lkey = 15;
+    //if X is to the right
+    else if(LinX > 575){
+      //three cases for Y
+      if(LinY < 470){
+        Lkey = 13;
+      }
+      else if(LinY > 575){
+        Lkey = 11;
+      }
+      else{
+        Lkey = 12;
+      }
     }
-    else if(LinY > 575){
-      Lkey = 17;
-    }
-    else{
-      Lkey = 16;
-    }
-  }
 
-  //if X is to the right
-  else if(LinX > 575){
-    //three cases for Y
-    if(LinY < 470){
-      Lkey = 13;
-    }
-    else if(LinY > 575){
-      Lkey = 11;
-    }
+    //if X is in the middle
     else{
-      Lkey = 12;
-    }   
-  }
+      //three cases for Y
+      if(LinY < 470){
+        Lkey = 14;
+      }
+      else if(LinY > 575){
+        Lkey = 10;
+      }
+      else{
+        Lkey = 00;
+      }
+    }
 
-  //if X is in the middle
-  else{
-    //three cases for Y
-    if(LinY < 470){
-      Lkey = 14;
-    }
-    else if(LinY > 575){
-      Lkey = 10;
-    }
-    else{
-      Lkey = 00;
-    }
-  }
-  
+  */
   //RIGHT STICK: if X is to the left
-  if(RinX < 470){  //about 60 (~20%) away from center to constitute change
+  if (RinX < 470) { //about 60 (~20%) away from center to constitute change
     //three cases for Y
-    if(RinY < 470){
+    if (RinY < 470) {
       Rkey = 25;
     }
-    else if(RinY > 575){
+    else if (RinY > 575) {
       Rkey = 27;
     }
-    else{
+    else {
       Rkey = 26;
     }
   }
 
   //if X is to the right
-  else if(RinX > 575){
+  else if (RinX > 575) {
     //three cases for Y
-    if(RinY < 470){
+    if (RinY < 470) {
       Rkey = 23;
     }
-    else if(RinY > 575){
+    else if (RinY > 575) {
       Rkey = 21;
     }
-    else{
+    else {
       Rkey = 22;
-    }   
+    }
   }
 
   //if X is in the middle
-  else{
+  else {
     //three cases for Y
-    if(RinY < 470){
+    if (RinY < 470) {
       Rkey = 24;
     }
-    else if(RinY > 575){
+    else if (RinY > 575) {
       Rkey = 20;
     }
-    else{
+    else {
       Rkey = 00;
     }
   }
-  
 
-  //Check pushbuttons, account for debouncing
-  if(chk_buttons(3)){
-    CAPS = CAPS ^ 1;
-  }
-  else if(chk_buttons(4)){
-    Shift = 1;
-  }
-  else if(chk_buttons(5)){
-    Ctrl = 1;
-  }
-  else if(chk_buttons(6)){
-    Alt = 1;
-  }
-
+  /*
+    //Check pushbuttons, account for debouncing
+    if(chk_buttons(3)){
+      CAPS = CAPS ^ 1;
+    }
+    else if(chk_buttons(4)){
+      Shift = 1;
+    }
+    else if(chk_buttons(5)){
+      Ctrl = 1;
+    }
+    else if(chk_buttons(6)){
+      Alt = 1;
+    }
+  */
   //Print button, joystick, and keyboard output maximum of 100ms (10Hz). After, reset button status (besides CAPS)
-  if(millis() >= (timepass + 100)){
+  if (millis() >= (timepass + 100)) {
     timepass = millis();    //set timer to next increment
-/*
+
     //joystick info
     Serial.print("LCoord: [");
     Serial.print(LinX, DEC);
@@ -209,15 +242,15 @@ void loop() {
     Serial.print(Lclick, DEC);
     Serial.print("  R-Click: ");
     Serial.print(Rclick, DEC);
-    Serial.print("  CAPS: ");
-    Serial.print(CAPS, DEC);
-    Serial.print("  Shift: ");
-    Serial.print(Shift, DEC);
-    Serial.print("  Ctrl: ");
-    Serial.print(Ctrl, DEC);
-    Serial.print("  Alt: ");
-    Serial.println(Alt, DEC);
-*/
+    /*    Serial.print("  CAPS: ");
+        Serial.print(CAPS, DEC);
+        Serial.print("  Shift: ");
+        Serial.print(Shift, DEC);
+        Serial.print("  Ctrl: ");
+        Serial.print(Ctrl, DEC);
+        Serial.print("  Alt: ");
+        Serial.println(Alt, DEC);
+    */
     //Reset all non-CAPS buttons
     Lclick = 0;
     Rclick = 0;
